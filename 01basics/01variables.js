90180a48a897627689488af28a60b6348a4962bf
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-06-18 12:18:25 +0000

    Datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,20 @@
+const accountId = 280
+let accountEmail = "harsh@google.com"
+var accountPassword = "harshraj"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 12 // ye kbhi update ni ho skta permanently set he
+
+console.log(accountId);
+
+accountEmail="h@gmail.com"
+accountPassword="12345"
+accountCity="Indore"
+
+/*
+Prefer not to use Var keyword.
+Because it creates the issue of block scope and functional scope.
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,28 @@
+"Use Strict"; // treat all the JS code as a New version
+
+//alert(3+3) // we are using node JS not browser so we cann't do this type operations.
+
+console.log(3
+    +3
+)   // Avoid invalid spacing and focus on Code Readability
+
+// two Documentations which we will use 1. MDN 2.tc93
+
+let harsh = "Bhat"
+
+// DataTypes
+
+// number => 2 to Power 53 iski range hee
+// bigint
+// string => ""
+// Boolean => true/false
+// null => stand alone value
+// undefined =>
+// Symbol => used for Unique categorization
+
+// Object => iska object bnake use krte he k=harsh esee.
+
+console.log(typeof harsh)
+
+console.log(typeof null); //iska output Object hota hee.
+console.log(typeof undefined); //iska output Undefined hi hota hee.
