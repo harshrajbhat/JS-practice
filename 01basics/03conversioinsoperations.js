dd5e4ebe1e74f937c9b9c0a1b0f02d6810a7d80d
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-06-19 11:43:00 +0000

    Conversion of DataTypes

---
+++ 01basics/03conversioinsoperations.js
@@ -0,0 +1,36 @@
+/*let score = "33"
+console.log(typeof score);
+
+let scoreinnum = Number(score)
+console.log(typeof scoreinnum);
+
+let sc = "123abc"
+
+let scinnum = Number(sc)
+console.log(typeof scinnum);
+console.log(scinnum);
+*/
+
+
+// "33" => 33
+// "33abc" => Not a Number(NaN); type abhi number hi rahega
+// null => 0
+// true =>1; false =>0
+// undefined => NaN
+
+/*let bull = 1
+
+let bullinboolean = Boolean(bull)
+console.log(bullinboolean);
+*/
+
+// 1 => true; 0 => false
+// "" => false
+// "Harsh" => true
+
+
+let num = 28
+
+let numinstring = String(num)
+console.log(numinstring);
+console.log(typeof numinstring);
