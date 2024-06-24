b2f91d76073aecdd0c0871c98ff7adb301334adf
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-06-24 08:19:27 +0000

    nums and maths

---
+++ 01basics/06nums_and_maths.js
@@ -0,0 +1,12 @@
+const score = 66
+// console.log(score);
+
+const scorenum = new Number(84.234) //trough New keyword we can specify datatype.
+// console.log(scorenum);
+
+// console.log(scorenum.toString().length);    //converts number to string.
+// console.log(scorenum.toFixed(1));           //gives fixed numbers after decimal
+// console.log(scorenum.toPrecision(4));       //gives the total number of values in a number
+
+const num = 1000000
+// console.log(num.toLocaleString('en-IN'));     //to convert the number with comas(,) in indian manner
