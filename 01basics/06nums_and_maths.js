771448d1b080edc973fedf010f02dc5a88667a76
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-06-24 08:36:54 +0000

    nums and maths

--- 01basics/06nums_and_maths.js
+++ 01basics/06nums_and_maths.js
@@ -10,3 +10,15 @@ const scorenum = new Number(84.234) //trough New keyword we can specify datatype
 
 const num = 1000000
 // console.log(num.toLocaleString('en-IN'));     //to convert the number with comas(,) in indian manner
+
+
+// +++++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.round(9.4));       //Rounds the value after the decimal
+// console.log(Math.abs(-9));      // converts negative value into same positive value
+// console.log(Math.ceil(9.4));        //rounds up and output 10
+// console.log(Math.floor(9.4));       //rounds up and output 9
+// console.log(Math.min(34,6,11,64));    //gives minimum number from array
+// console.log(Math.max(34,6,11,64));    //gives maximum number from array
+
+console.log(Math.random());
