ff123dad6fadc1f7633445c2ef421b89cedde31d
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-06-27 10:31:20 +0000

    Scopes Done

--- 03basics/02scope.js
+++ 03basics/02scope.js
@@ -2,10 +2,43 @@ let a = 28  //GLOBAL SCOPE
 if (true){
     let a = 1   //BLOCK SCOPE
     const b = 2 
-    console.log("INNER A is : ",a);
+    // console.log("INNER A is : ",a);
     // var c = 3
 }
 // console.log(a); //ye print ni hoga
 // console.log(b); //ye bhi print ni hoga
 // console.log(c); //ye var he isliye khi bhi print hojayega bkl.
-console.log(a);
+// console.log(a);
+
+function one() {
+   const username ="Harsh"
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true){
+    const username = "Harry"
+    if (username === "Harry"){
+        const website = " Youtube"
+        // console.log(username + website);
+         
+    }
+    // console.log(website);
+}
+
+//+++++++++++++++++++++++++++++++++++++++INTERESTING++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// addone()        //ye chal jayega
+function addone(num) {
+    return num + 1
+}
+
+// console.log(addtwo())       //ye declaration nhi chlegaa
+const addtwo = function (num) {
+    return num+2
+}
