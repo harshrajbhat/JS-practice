786b112bb8202db2070e2f16494e5845364202e8
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-07-22 14:29:52 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if-else keyword
+
+// comparision operators :-  <, >, <=, >=, ==, ===, !=
+
+const isUserLoggedIn = true
+const teampreature = 41
+
+// if (teampreature > 40){
+//     console.log("teampreature is high ")
+// }
+// else{
+//     console.log("teampreature is low ");
+// }
+
+const score = 200
+// if (score >101){
+//     const power = "fly"
+//     console.log(`User power is to ${power}`);
+// }
+
+
+// SHORT HAND CODE
+
+const balance = 2000
+// if (balance>1400) console.log("You are Rich");
+
+// NESTING
+
+if (balance<500)
