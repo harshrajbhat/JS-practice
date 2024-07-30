94a122a846fb236d8a0a9c4cc29651db0b8a886e
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-07-30 12:21:47 +0000

    for of loop

---
+++ 05itreations/03_loops.js
@@ -0,0 +1,29 @@
+// for of loop
+
+// ["","",""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello world"
+for (const greeet of greetings) {
+    // console.log("each char is " + greeet);
+}
+
+
+// Maps :- same like object but keeps unique values and store in exact manner in which it is entered.
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('FR', 'France')
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, " :- ", value);
+}
