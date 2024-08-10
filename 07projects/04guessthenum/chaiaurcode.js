832e9eeb3efc3d541d6e8ac93de3c3816ee61216
harshrajbhat <142987000+harshrajbhat@users.noreply.github.com>
2024-08-10 11:38:26 +0000

    project 4

---
+++ 07projects/04guessthenum/chaiaurcode.js
@@ -0,0 +1,41 @@
+const randomNum = (parseInt(Math.random() *100 + 1));
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrhigh = document.querySelector('.lowOrhi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+let playGame = true
+
+if(playGame)
+
+function validateGuess(guess) {
+        // 
+}
+
+function checkGuess(guess) {
+    // 
+}
+
+function displayGuess(guess) {
+    // 
+}
+
+function displayMessage(message) {
+    // 
+}
+
+function endGame() {
+    
+}
+
+function newGame() {
+    
+}
+
